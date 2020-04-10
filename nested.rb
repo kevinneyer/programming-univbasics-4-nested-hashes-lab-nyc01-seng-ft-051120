f0fdca727f538680803677e6d278506839ae155e
Kevin Neyer <kevinneyer@gmail.com>
2020-04-10 20:38:36 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -55,7 +55,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-programmer_hash[:dennis_ritche][:languages] = Array.to_s 
+programmer_hash[:dennis_ritche][:languages][0] = .to_s 
 end 
 
 def adding_matz
