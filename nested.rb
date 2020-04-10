dd48121a3af38f2d9f04dd02345723565ada4de9
Kevin Neyer <kevinneyer@gmail.com>
2020-04-10 20:47:36 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
         :languages => ["C"]
       }
     }
-
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -36,7 +36,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,8 +55,9 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
-end
+yo = programmer_hash[:dennis_ritchie][:languages][0]
+yo.to_s 
+end 
 
 def adding_matz
 # add the following information to the top level of programmer_hash
@@ -81,7 +82,12 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+programmer_hash[:yukihiro_matsumoto] = {
+ :known_for => "Ruby",
+ :languages => ["LISP", "C"]
+}
+ 
+return programmer_hash
 end
 
 def changing_alan
@@ -103,7 +109,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+programmer_hash[:alan_kay][:known_for] = "GUI"
+programmer_hash
 end
 
 def adding_to_dennis
@@ -125,5 +132,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+programmer_hash
 end
